c77cf2625136ed0932a704331af678db4881ac33
Salman Mallick <salman.mallick@parsus.com>
2019-11-12 14:39:45 -0700

    fix(confirmation): renamed thank you page and used the default method/action for forms.

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,3 @@
 (function ($) {
 	$('.transportation-picker').selectpicker();
-
-	$('.av-form-inquiry').submit(function (event) {
-
-		var $form = $(this).serialize();
-
-		$.post('./thank-you.html', {data: $form});
-
-	});
-
-
-
 })(jQuery);
