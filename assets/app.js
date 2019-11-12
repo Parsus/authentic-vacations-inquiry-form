0bf7ae5a2f7298a070b29ea729799938c8cc2c9b
Salman Mallick <salman.mallick@parsus.com>
2019-11-12 14:00:45 -0700

    feat(thank-you): Added a thank you page on form submit,

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
 (function ($) {
 	$('.transportation-picker').selectpicker();
+
+	$('.av-form-inquiry').submit(function (event) {
+		event.preventDefault();
+
+		var $form = $(this).serialize();
+
+		$.post('./thank-you.html', {data: $form});
+
+	});
+
+
+
 })(jQuery);
