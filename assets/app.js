b5d6f21d41402dd015d9fb3b007ef5cf48310ff3
Salman Mallick <salman.mallick@parsus.com>
2019-11-19 15:31:42 -0700

    feat(app.js): added JS to uncheck/deselect checkbox/night dropdown based on the others input

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,16 @@
 (function ($) {
 	$('.transportation-picker').selectpicker();
+
+	$('input[name="notSureYet"').change(function () {
+		if ($('input[name="notSureYet"').is(":checked")) {
+			$('select[name="nights"').val("");
+		}
+	});
+
+	$('select[name="nights"').change(function () {
+		if ($('input[name="notSureYet"').is(":checked") && $('select[name="nights"').val() != "") {
+			$('input[name="notSureYet"').prop('checked',false);
+		}
+	});
+
 })(jQuery);
